6d58782ef35319d8a51e6ea6145c673fa48656ac
Trang Nguyen <nguyenminhtrang9213@gmail.com>
2021-09-26 12:02:57 +0200

    Setting default  video player

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,13 @@ const VideoList = ({ videos, onVideoSelect }) => {
   //props.videos
 
   const renderedList = videos.map((video) => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
   return <div className="ui relaxed divided list ">{renderedList}</div>;
 };
