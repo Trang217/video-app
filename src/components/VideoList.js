85fed56f2d595c2d45fc2990c20581d83d121579
Trang Nguyen <nguyenminhtrang9213@gmail.com>
2021-09-26 10:39:09 +0200

    Adding Video list

---
+++ src/components/VideoList.js
@@ -0,0 +1,12 @@
+import VideoItem from "./VideoItem";
+
+const VideoList = ({ videos }) => {
+  //props.videos
+
+  const renderedList = videos.map((video) => {
+    return <VideoItem video={video} />;
+  });
+  return <div className="ui relaxed divided list ">{renderedList}</div>;
+};
+
+export default VideoList;
